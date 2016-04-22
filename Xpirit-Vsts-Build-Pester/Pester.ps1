cbc16d1caa912d8af58bf4805bb67730244657d4
peter groenewegen <pgroene@gmail.com>
2016-04-22 11:29:30 +0200

    Fixed running on default agent Multiple runs support in outputfile naming Check if pester is installed

--- Xpirit-Vsts-Build-Pester/Pester.ps1
+++ Xpirit-Vsts-Build-Pester/Pester.ps1
@@ -1,20 +1,54 @@
     Param(
     [string] $ItemSpec = "**/*.tests.ps1"
 )
+function Get-ModuleVersion($modulename){
+    return (Get-Module -Name $modulename).Version
+}
 
-#Write-Output "ItemSpec: $ItemSpec" 
-#Write-Output "Temp folder: $env:temp"
+Write-Output "ItemSpec: $ItemSpec" 
 
-$tempFile = Join-Path $env:temp pester.zip
-$modulePath = Join-Path $env:temp pester-master\Pester.psm1
+$pesterversion = Get-ModuleVersion ("Pester")
+if ($pesterversion) {
+    #pester is installed on the system
+	Write-Output "Pester is installed $pesterversion"
+} else {
+	Write-Output "Intalling latest version of pester"
+    
+    #install pester
+    $tempFile = Join-Path $env:temp "pester.zip"
+	$modulePath = Join-Path $env:temp "pester-master\Pester.psm1" 
 
-Invoke-WebRequest https://github.com/pester/Pester/archive/master.zip -OutFile $tempFile
+	Invoke-WebRequest https://github.com/pester/Pester/archive/master.zip -OutFile $tempFile
  
-Add-Type -Assembly System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
+	$unzipdir = Join-Path $env:temp "pester-master"
+	if (Test-Path $unzipdir){
+	    Remove-Item "$unzipdir" -recurse
+	}
 
-Import-Module $modulePath -DisableNameChecking -Verbose
+	Add-Type -Assembly System.IO.Compression.FileSystem
+	[System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
 
-$outputFile = Join-Path $env:BUILD_SOURCESDIRECTORY "TEST-pester.xml"
+	Import-Module $modulePath -DisableNameChecking -Verbose
+
+    $pesterversion = Get-ModuleVersion ("Pester")
+	Write-Output "Pester installed $pesterversion"
+}
+
+[string] $filepart1 = "TEST-pester"
+[string] $filepart2 = ".xml"
+[string] $filename = -Join ($filepart1, $filepart2)
+
+$outputFile = Join-Path $env:COMMON_TESTRESULTSDIRECTORY $filename
+
+if (Test-Path $outputFile){
+    [int]$counter = 1
+	while (Test-Path $outputFile){
+        $filename = -Join ($filepart1,  (-Join ($counter, $filepart2)))
+		$outputFile = Join-Path $env:COMMON_TESTRESULTSDIRECTORY $filename
+        $counter = $counter+1
+	}
+}
+
+Write-Output "Writing pester output to $outputfile"
 
 Invoke-Pester $ItemSpec -PassThru -Outputformat nunitxml -Outputfile $outputFile
